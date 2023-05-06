106138df055015713392c2ad99a86ed96f526a60
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-05-06 09:07:25 +0000

    [Add] 2023/05/06 プロフィール編集画面、ユーザ退会処理作成etc

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,19 +9,55 @@ class Public::UsersController < ApplicationController
 
   end
 
-  # プロフィール編集
+  # プロフィール編集画面
   def edit
 
     @user = user_find
 
   end
 
+  # プロフィール更新
   def update
+
+    @user = user_find
+
+    if @user.update(user_params)
+
+      notice("プロフィールを更新しました。")
+      redirect_to my_page_path
+
+    else
+
+      render :edit
+
+    end
+
   end
 
+  # 退会確認画面
   def unsubscribe
   end
 
+  # 退会処理
   def goodbye
+
+    @user = user_find
+    @user.update(is_deleted: true)
+    # ログアウト
+    reset_session
+
+    notice("退会完了いたしました。またのご利用お待ちしております。")
+    redirect_to root_path
+
   end
+
+  private
+
+  # ストロングパラメータ
+  def user_params
+
+    params.require(:user).permit(:name,:sex,:email,:self_introduction,:image)
+
+  end
+
 end
