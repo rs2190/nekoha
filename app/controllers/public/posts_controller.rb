feda33fa5a73a1bc9d53bec80e8236b129b412b6
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-05-17 12:18:14 +0000

    [Add] 2023/05/17 投稿機能etc

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,14 +1,43 @@
 class Public::PostsController < ApplicationController
+
   def index
+
+    @posts = Post.where(user_id: current_user.id).page(params[:page]).per(10).order(id: "ASC")
+
   end
 
+  # 投稿内容登録画面
   def new
+
+    # データを受け取り新規登録するためのインスタンス作成
+    @post = Post.new
+
   end
 
+  # 投稿内容登録処理
   def create
+
+    # データを受け取り新規登録するためのインスタンス作成
+    @post = Post.new(post_params)
+    # Post.ユーザーID = ログインしたログインした顧客のid
+    @post.user_id = current_user.id
+
+    # 登録時チェック
+    if @post.save
+      # 成功時
+      notice("投稿完了しました。")
+      redirect_to post_path(@post.id)
+    else
+
+      render :new
+    end
+
   end
 
   def edit
+
+     @posts = Post.find(user_id: current_user.id)
+
   end
 
   def update
@@ -16,4 +45,15 @@ class Public::PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  # ストロングパラメータ
+  def post_params
+
+    params.require(:post).permit(:image,:title,:posts_comment)
+
+  end
+
+
 end
