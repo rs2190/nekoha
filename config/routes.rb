8707ff98ed1e820d8946089ba59cdac338181df1
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-06-25 15:47:21 +0000

    [Update] 2023/06/26 routes.rbコメント追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   # URL /users/sign_in ...
 
   devise_scope :user do
+    # ゲストログイン
     post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in',as: 'guest_sign_in'
   end
 
   devise_for :users,skip: [:passwords], controllers: {
 
+    # ユーザー新規登録画面
     registrations: "public/registrations",
+    # ログイン画面
     sessions: 'public/sessions'
 
   }
@@ -33,18 +36,26 @@ Rails.application.routes.draw do
     patch 'users/goodbye' => 'users#goodbye', as: 'goodbye'
     # ユーザー詳細画面
     resources :users, only: [:show] do
+
+      # ユーザーの投稿内容一覧
       get 'posts'
+      # ユーザーのコメント一覧
       get 'comments'
+       # ユーザーのいいね一覧
       get 'favorites'
     end
 
-    # 投稿内容一覧（ユーザー投稿全て）
-    get 'posts/user_index' => 'posts#user_index', as: 'user_index'
+    # 投稿内容一覧（ユーザー投稿全て）⇒　削除
+    # get 'posts/user_index' => 'posts#user_index', as: 'user_index'
     resources :posts do
+
+      # コメント機能
       resources :comments, only: [:create,:edit,:update,:destroy]
+      # いいね機能
       resources :favorites, only: [:create, :destroy]
     end
 
+    # ねこずかん（種類一覧）系
     resources :types, only: [:index, :show]
 
   end
@@ -52,6 +63,8 @@ Rails.application.routes.draw do
     # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+
+    # 管理者ログイン
     sessions: "admin/sessions"
   }
 
@@ -60,9 +73,13 @@ Rails.application.routes.draw do
 
   namespace :admin do
 
+    # ユーザー一覧系
     resources :users, only: [:index, :show, :edit, :update]
+    # 投稿内容一覧系
     resources :posts, only: [:show, :destroy]
+    # コメント一覧系
     resources :comments, only: [:index, :destroy]
+    # ねこずかん（種類一覧）系
     resources :types
 
   end
