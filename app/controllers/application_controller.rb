8baa1e3839a77f3122688bbedd3bf9b61b958261
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-06-28 10:57:24 +0000

    [Update] 2023/06/28 コメント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に、configure_permitted_parametersメソッドが実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # サインアウト後の遷移
