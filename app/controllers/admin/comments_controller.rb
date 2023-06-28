cff7b37bcde5c1daeac1c66f9c63be6e8970eb21
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-06-28 11:37:45 +0000

    [Update] 2023/06/28 コメント修正

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,12 @@
 class Admin::CommentsController < ApplicationController
 
+  # コントローラーに設定して、ログイン済管理者のみにアクセスを許可する
   before_action :authenticate_admin!
 
   # コメント一覧
   def index
 
+    # コメント欄30件取得（降順）
     @comments = Comment.page(params[:page]).per(30).order(id: "DESC")
 
   end
@@ -12,9 +14,13 @@ class Admin::CommentsController < ApplicationController
   # コメント削除機能
   def destroy
 
+    # 1件コメント取得
     comments = comment_find_params_id
+    # 削除
     comments.destroy
+    # フラッシュメッセージ
     notice("コメントを削除しました。")
+    # 遷移先
     redirect_to admin_comments_path
 
   end
