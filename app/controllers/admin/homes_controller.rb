eb26ca437d2d869cbadf4880f1a9e93c03cf8acc
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-06-29 12:11:49 +0000

    [Update] 2023/06/29 コメント修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -5,6 +5,7 @@ class Admin::HomesController < ApplicationController
   # トップページ
   def top
 
+    # 投稿モデルから1ページ分（データ30件）取得（降順）
     @posts = Post.page(params[:page]).per(30).order(id: "DESC")
 
   end
