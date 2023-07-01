9a9b6475b71269d42804c6492c954ade08da53bf
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-07-01 12:01:54 +0000

    [Ypdate] 2023/07/01 コメント追加実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PostsController < ApplicationController
 
+  # コントローラーに設定して、ログイン済管理者のみにアクセスを許可する
   before_action :authenticate_admin!
 
   # 投稿内容詳細画面
@@ -12,9 +13,13 @@ class Admin::PostsController < ApplicationController
   # 投稿内容削除処理
   def destroy
 
+    # 1件投稿情報取得
     @post = post_find_params_id
+    # 削除
     @post.destroy
+    # フラッシュメッセージ
     notice("投稿内容を削除しました。")
+    # 処理後遷移先
     redirect_to admin_home_top_path
 
   end
