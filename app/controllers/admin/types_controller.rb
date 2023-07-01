417a48dfc8cf9224c38279fccea8e54b1265640b
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-07-01 12:18:20 +0000

    [Update] 2023/07/01 コメント追加実装

--- app/controllers/admin/types_controller.rb
+++ app/controllers/admin/types_controller.rb
@@ -1,10 +1,12 @@
 class Admin::TypesController < ApplicationController
 
+  # コントローラーに設定して、ログイン済管理者のみにアクセスを許可する
   before_action :authenticate_admin!
 
   # ねこずかん（種類一覧）
   def index
 
+    # 種類モデルから1ページ分（データ30件）取得（降順）
     @types = Type.page(params[:page]).per(30).order(id: "DESC")
 
   end
@@ -24,11 +26,16 @@ class Admin::TypesController < ApplicationController
     @type = Type.new(type_params)
     # 登録時チェック
     if @type.save
-      # 成功時
+
+      # 成功時処理
+      # フラッシュメッセージ
       notice("ねこずかんを追加しました。")
+      # 処理後遷移先
       redirect_to admin_type_path(@type.id)
     else
 
+      # 失敗時処理
+      # 処理後遷移先
       render :new
     end
 
@@ -37,6 +44,7 @@ class Admin::TypesController < ApplicationController
   # 種類詳細画面
   def show
 
+    # 1件種類情報取得
     @type = type_find_params_id
 
   end
@@ -44,6 +52,7 @@ class Admin::TypesController < ApplicationController
   # 種類情報の更新処理
   def edit
 
+    # 1件種類情報取得
     @type = type_find_params_id
 
   end
@@ -51,15 +60,22 @@ class Admin::TypesController < ApplicationController
   # 種類情報の更新処理
   def update
 
+    # 1件種類情報取得
     @type = type_find_params_id
 
+    # 更新時チェック
     if @type.update(type_params)
 
+      # 成功時処理
+      # フラッシュメッセージ
       notice("ねこずかんを更新しました。")
+      # 処理後遷移先
       redirect_to admin_type_path(@type.id)
 
     else
 
+      # 失敗時処理
+      # 処理後遷移先
       render :edit
 
     end
@@ -69,9 +85,13 @@ class Admin::TypesController < ApplicationController
   # 種類情報の更新処理
   def destroy
 
+    # 1件種類情報取得
     type = type_find_params_id
+    # 削除
     type.destroy
+    # フラッシュメッセージ
     notice("ねこずかんから１件削除しました。")
+    # 処理後遷移先
     redirect_to admin_types_path
 
   end
