24b5619550038caceab029ec68a0377a08febcdd
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-07-01 12:26:25 +0000

    [Update] 2023/07/01 コメント追加実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,12 @@
 class Admin::UsersController < ApplicationController
 
+  # コントローラーに設定して、ログイン済管理者のみにアクセスを許可する
   before_action :authenticate_admin!
 
+  # ユーザー一覧
   def index
 
+    # ユーザーモデルから1ページ分（データ30件）取得（降順）
     @users = User.page(params[:page]).per(30).order(id: "DESC")
 
   end
@@ -11,6 +14,7 @@ class Admin::UsersController < ApplicationController
   # ユーザー詳細
   def show
 
+    # 1件ユーザー情報取得
     @user = user_find_params_id
 
   end
@@ -18,6 +22,7 @@ class Admin::UsersController < ApplicationController
   # ユーザー編集画面
   def edit
 
+    # 1件ユーザー情報取得
     @user = user_find_params_id
 
   end
@@ -25,19 +30,25 @@ class Admin::UsersController < ApplicationController
   # ユーザー情報更新
   def update
 
+    # 1件ユーザー情報取得
     @user = user_find_params_id
 
+    # 更新時チェック
     if @user.update(user_params)
 
+      # 成功時処理
+      # フラッシュメッセージ
       notice("対象のユーザー情報を更新しました。")
+      # 処理後遷移先
       redirect_to admin_user_path(@user.id)
 
     else
 
+      # 失敗時処理
+      # 処理後遷移先
       render :edit
 
     end
-
 
   end
 
