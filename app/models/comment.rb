92a9e953586b6aebbbb1d1246bfd2911b5cdc2cf
ryu_suzuki <122139610+rs2190@users.noreply.github.com>
2023-09-02 08:36:14 +0000

    [Update] 2023/09/02 コメント追加実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ApplicationRecord
   # comment と post の 1 : N のアソシエーション実装
   belongs_to :post
 
-  # バリデーション
+  # バリデーション(入力必須のバリデーションをまとめる。)
   with_options presence: true do
 
     validates :comment_word
@@ -15,6 +15,7 @@ class Comment < ApplicationRecord
   # コメント日時
   def get_comment_date
 
+    # 日時フォーマットを修正
     created_at.strftime('%Y/%m/%d %H:%M')
 
   end
@@ -22,11 +23,13 @@ class Comment < ApplicationRecord
     # コメント日時
   def get_comment_word
 
+    # 10文字以上の場合
     if comment_word.length > 10
 
       # 最初の10文字取得
       word = comment_word.slice(0..10)
       word + "..."
+
     else
 
       comment_word
@@ -38,6 +41,7 @@ class Comment < ApplicationRecord
   # タイトル取得
   def get_post_title(post_id)
 
+    # 投稿IDで検索して、0番目のタイトルを取得する。
     Post.where(id: post_id).pluck(:title)[0]
 
   end
@@ -45,15 +49,17 @@ class Comment < ApplicationRecord
   # 投稿者ID取得
   def get_post_user_id(post_id)
 
+    # 投稿IDで検索して、0番目のユーザーIDを取得する。
     Post.where(id: post_id).pluck(:user_id)[0]
 
   end
 
-  # 投稿ユーザー取得
+  # 投稿ユーザーの取得
   def get_post_user_name(post_id)
 
+    # 投稿モデルのユーザーIDを取得。
     post_user_id = get_post_user_id(post_id)
-
+    # 取得したユーザーIDで、ユーザーネームを取得。
     User.where(id: post_user_id).pluck(:name)[0]
 
   end
