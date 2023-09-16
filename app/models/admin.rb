db36f383aef7a25648dac7d8c6405b27deee3ccf
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-09-16 12:16:22 +0000

    [Update] 2023/09/16 コメント追加実装

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,7 @@
 class Admin < ApplicationRecord
+
+  # device 使用
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
